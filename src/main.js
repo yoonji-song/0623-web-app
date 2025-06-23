e918c7abd7d228ae34a4f28d453d8bdc2d8560f1
yoonji-song <songyj@snu.ac.kr>
2025-06-23 11:47:11 +0900

    sweetalert2

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
+import Swal from 'sweetalert2';
+
 document.addEventListener("DOMContentLoaded", () => {
   const button = document.getElementById("dangerButton");
 
   button.addEventListener("click", () => {
-    alert("클릭하지 마세요!");
+    Swal.fire({
+      title: '경고!',
+      text: '클릭하지 말라고 했잖아요!',
+      icon: 'warning',
+      confirmButtonText: '알겠어요 😓',
+      confirmButtonColor: '#66bb6a',
+    });
   });
 });
